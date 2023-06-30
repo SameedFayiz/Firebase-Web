0e20bb9f8e15ab045f5216ba7ea9b030d636eb72
SameedFayiz <sameedfayiz@gmail.com>
2023-06-30 22:08:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ onAuthStateChanged(auth, (user) => {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
         const uid = user.uid;
-        location.href = "/home/home.html"
+        location.href = window.location.pathname + "home/home.html"
 
     } else {
         // User is signed out
